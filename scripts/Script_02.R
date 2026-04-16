f5a522c2a459670f3a1409d3e5e6e8ad48e35975
Alwl-pixel <121455752+Alwl-pixel@users.noreply.github.com>
2026-04-16 23:55:22 +0100

    Add files via upload

--- scripts/Script_02.R
+++ scripts/Script_02.R
@@ -8,8 +8,7 @@ library(lubridate)
 # 1) import
 patients <- read_csv("data/patients.csv")
 admissions <- read_csv("data/admissions.csv")
-labs <- read_csv("data/labs.csv")
-rx <- read_csv("data/prescriptions.csv")
+
 
 # 2) quick look
 glimpse(patients)
@@ -33,43 +32,27 @@ patients_clean <- patients %>%
     )
   )
 
-#admissions_clean <- admissions %>%
-  mutate(
-    admit_dt = ymd_hm(admit_datetime),
-    discharge_dt = ymd_hm(discharge_datetime),
-    # flag impossible dates
-    discharge_before_admit = discharge_dt < admit_dt
-  )
 
-# 4) joins
-adm_with_dx <- admissions_clean %>%
-  left_join(select(patients_clean, patient_id, index_diagnosis), by = "patient_id")
+# 4) simple summary
+patients_clean %>%
+  count(index_diagnosis, sort = TRUE)
 
-# 5) simple summary
-adm_with_dx %>%
-  count(index_diagnosis, ward, sort = TRUE)
 
-# 6) labs cleaning (convert messy strings)
-labs_clean <- labs %>%
-  mutate(
-    value_num = parse_number(as.character(value)),
-    collected_dt = ymd_hm(collected_datetime)
-  )
 
-# 7) basic plot
+# 5) basic plot
 patients_clean %>%
   ggplot(aes(x = age)) +
   geom_histogram(binwidth = 5) +
   labs(title = "Age distribution (synthetic)")
 
-# 8) mini example of the data science life cycle
+# 6) mini example of the data science life cycle
 patients_clean %>%
    group_by(index_diagnosis) %>%
   summarise(
    n = n(),
     crp_median = median(crp_admission_mg_l, na.rm = TRUE))%>% arrange(desc(crp_median))
 
-#visualisation of mini example
+# 7) visualisation of mini example
 patients_clean %>%
   ggplot(aes(x = index_diagnosis, y = crp_admission_mg_l)) +
   geom_boxplot() +
